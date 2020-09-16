09a49ede2bf51a122f49946822ef489db0a9923e
Si-hub <zweto.dayile@gmail.com>
2020-09-16 12:43:32 +0200

    added text to my files

--- greetings.js
+++ greetings.js
@@ -1,71 +1,84 @@
-module.exports = function greetings(){
-    var object =  {};
- 
-  function errorMessage(){
-    if( userName === "" && language === null){
-        return  "please enter your name and select language"
+module.exports = function greetings() {
+    var object = {};
+
+    function errorMessage() {
+        if (userName === "") {
+            return "please enter your name and select language"
+        }
     }
-  }
-    function greet(name, language){
-        
+    function greet(name, language) {
+
         //if you forgot to type a name but you did select a language do the following//
-        if(!name){
-            if (language === "IsiXhosa"){
-                return "Nceda ufake igama lakho" 
+        if (!name) {
+            if (language === "IsiXhosa") {
+                return "Nceda ufake igama lakho"
             }
-            if (language === "English"){               
+            if (language === "English") {
                 return "Please put your name"
             }
-            if (language === "Afrikaans"){
+            if (language === "Afrikaans") {
                 return "Plaas jou naam"
-            } 
+            }
         }
 
         var take = name.toLowerCase();   //convert all string typed in lower cases 
-        if(object[take] === undefined){  //"undefined" if someone didnt put anything 
+        if (object[take] === undefined) {  //"undefined" if someone didnt put anything 
             object[take] = 0;            // dont increase the counter
         }
-        if(object[take] === 1){
+        if (object[take] === 1) {
 
         }
-        else{object[take] += 1;
+        else {
+            object[take] += 1;
         }
 
-        if (language === "IsiXhosa"){   // greet a person with his language followed by name
-            return "Molo " + name; 
+        if (language === "IsiXhosa") {   // greet a person with his language followed by name
+            return "Molo " + name;
         }
-        if (language === "English"){               
+        if (language === "English") {
             return "Good day " + name;
         }
-        if (language === "Afrikaans"){
+        if (language === "Afrikaans") {
             return "Goeie daag " + name;
-        } 
-    }    
-    
-    function getNames(){
+        }
+    }
+
+    function getNames() {
         return object;
     }
-                                         
-    function greetingsCounter(){
+
+    function greetingsCounter() {
         var calculate = 0;
-        for(var key in object){
-            if(object.hasOwnProperty(key)){  //use for...in statement to loop through 
-                calculate ++;                //the properties of an object
-                              // also use "hasOwnProperty" method to check whether if the
+        for (var key in object) {
+            if (object.hasOwnProperty(key)) {  //use for...in statement to loop through 
+                calculate++;                //the properties of an object
+                // also use "hasOwnProperty" method to check whether if the
             }               //property(key) belongs to the object
         }
         return calculate;
     }
 
-    function clean(){ //i will use this function to clean up my 
+    function eachUserCounter(name) {
+        // use for..in loop to iterate over an object (objects store properties)      
+        for (var key in object) { //"key" in obj: To check if a property with the given key exists 
+            if (key === name) {   
+              var element = object[key] //Square brackets allow us to take the key from a variable object                
+                
+            }                           //values that are inside the object
+        }
+        return element;
+    }
+
+    function clean() { //i will use this function to clean up my 
         object = {};  // localstorage with "localstorage.clear()"
     }
-    return{
+    return {
         errorMessage,
         greet,
         greetingsCounter,
         clean,
         getNames,
-        
+        eachUserCounter,
+
     }
 }
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //import a module by name
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const flash = require('express-flash');
+const session = require('express-session');
 const express = require("express");
 
 const greetings = require('./greetings');
@@ -10,9 +12,20 @@ const app = express();
 // function instance
 const Greetings = greetings();
 
+
 // view engine setup
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+// initialise session middleware - flash-express depends on it
+app.use(session({
+  secret: 'this is my long string that is used for session in http',
+  resave: false,
+  saveUninitialized: true
+}));
+
+// initialise the flash middleware
+app.use(flash());
 
 
 // for parsing application/x-www-form-urlencoded
@@ -25,8 +38,10 @@ app.use(express.static('public'))
 
 // Get
 app.get("/", function (req, res) {
-  // send the rendered view to the client
-  res.render("index");
+
+  res.render("index", {
+    title: 'Home'
+  });
 });
 
 //Post
@@ -34,35 +49,38 @@ app.post("/greetings", function (req, res) {
   var userName = req.body.userName;
   var language = req.body.language;
 
+  if (!userName && !language){
+    req.flash('info', 'please enter your name and select language');
+  }else if (!language){
+    req.flash('info', 'please select your language');
+  }
   //display my greetings
   const displayMyGreetings = Greetings.greet(userName, language)
 
   const greetingsCounter = req.body.greetingsCounter;
   const counter = Greetings.greetingsCounter(greetingsCounter)// dispaly my counter
-  console.log(Greetings.getNames())
+  //console.log(Greetings.getNames())
   res.render("index", {
     display: displayMyGreetings,
     count: counter
   });
 })
 
-
-app.get("/greeted", function (req, res) {
+app.get("/greeted", function (req, res) { // server receives an HTTP request, it will provide Http response
   var names = Greetings.getNames();
-  for (const list in names) {
-
+  for (const list in names) { // for...in loop: loop over the properties of the Object to find 
+    //names that has been greeted 
   }
-
   res.render("greet", { name: names });
 })
 
-app.get("/counter/:user_name", function (req, res) {
-    const user_name = req.params.user_name
-    var counter = Greetings.greetingsCounter(user_name);
+app.get("/counter/:userName", function (req, res) {
+  const userName = req.params.userName
+  var counter = Greetings.eachUserCounter(userName);
   res.render("counter", {
-    names : Greetings.getNames(),
-    user_name,
-     counter});
+    userName,
+    counter
+  });
 })
 
 app.post("/reset", function (req, res) {
