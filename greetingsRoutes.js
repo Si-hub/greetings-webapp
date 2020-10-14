cd55b3c36916ce6c78f011518c71c16b178e1ef9
Si-hub <zweto.dayile@gmail.com>
2020-10-14 14:45:46 +0200

    added a route file

---
+++ greetingsRoutes.js
@@ -0,0 +1,81 @@
+module.exports = function greetingRoute() {
+
+    const pg = require("pg");
+    const Pool = pg.Pool;
+
+    const connectionString = process.env.DATABASE_URL || 'postgresql://sim:pg123@localhost:5432/greet';
+
+    const pool = new Pool({
+        connectionString
+    });
+
+    const greetings = require('./greetings');
+    const Greetings = greetings(pool);
+
+    async function home(req, res) {
+
+        res.render("index");
+    }
+
+    async function toGreet(req, res) {
+        var userName = req.body.userName;
+        var language = req.body.language;
+        // console.log(req.body.userName)
+        // console.log(req.body.language)
+
+        if (!userName && !language) {
+            req.flash('info', 'please enter your name and select language');
+        } else if (!language) {
+            req.flash('info', 'please select your language');
+        } else {
+            // dispaly my counter
+            var counter = await Greetings.counter()
+
+            const check = await Greetings.checkName(userName)
+                // console.log("check " + check)
+            if (check === 0) {
+                Greetings.insertNames(userName)
+            } else {
+                Greetings.updateCounter(userName)
+            }
+            //display my greetings
+            var displayMyGreetings = await Greetings.greet(userName, language)
+                // Greetings.setName(userName)
+        }
+        //console.log({ counter, displayMyGreetings })
+        res.render("index", {
+            display: displayMyGreetings,
+            count: counter
+        });
+    }
+
+    async function namesGreeted(req, res) {
+        var names = await Greetings.getNames();
+
+        res.render("greet", { name: names });
+    }
+
+    async function eachUser(req, res) {
+        const userName = req.params.userName
+        var namesList = await Greetings.greetedUsersCount(userName)
+        let msg = 'Hello, ' + userName + ' has been greeted ' + namesList + ' times '
+        res.render("counter", {
+            message: msg
+        });
+    }
+
+    async function Reset(req, res) {
+        await Greetings.clearValues()
+        res.redirect("/")
+
+    }
+
+    return {
+        toGreet,
+        home,
+        namesGreeted,
+        eachUser,
+        Reset
+
+    }
+}
--- index.js
+++ index.js
@@ -4,20 +4,14 @@ const bodyParser = require('body-parser');
 const flash = require('express-flash');
 const session = require('express-session');
 const express = require("express");
+const routes = require('./greetingsRoutes');
 
-const pg = require("pg");
-const Pool = pg.Pool;
-
-const connectionString = process.env.DATABASE_URL || 'postgresql://sim:pg123@localhost:5432/greet';
-
-const pool = new Pool({
-    connectionString
-});
 
 // create my app
 const app = express();
-const greetings = require('./greetings');
-const Greetings = greetings(pool);
+const greetingsRoutes = require('./greetingsRoutes');
+
+const GreetingsRoutes = greetingsRoutes();
 
 
 
@@ -37,74 +31,23 @@ app.use(flash());
 
 
 // for parsing application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false })) // why extended false?
+app.use(bodyParser.urlencoded({ extended: false }))
     // for parsing application/json
 app.use(bodyParser.json())
 
 //this will make server instance to find css in the public folder
 app.use(express.static('public'))
 
-// Get
-app.get("/", async function(req, res) {
 
-    res.render("index", {
-        title: 'Home'
-    });
-});
+app.get("/", GreetingsRoutes.home);
 
-//Post
-app.post("/greetings", async function(req, res) {
-    var userName = req.body.userName;
-    var language = req.body.language;
-    // console.log(req.body.userName)
-    // console.log(req.body.language)
+app.post("/greetings", GreetingsRoutes.toGreet);
 
-    if (!userName && !language) {
-        req.flash('info', 'please enter your name and select language');
-    } else if (!language) {
-        req.flash('info', 'please select your language');
-    } else {
-        // dispaly my counter
-        var counter = await Greetings.counter()
+app.get("/greeted", GreetingsRoutes.namesGreeted);
 
-        const check = await Greetings.checkName(userName)
-            // console.log("check " + check)
-        if (check === 0) {
-            Greetings.insertNames(userName)
-        } else {
-            Greetings.updateCounter(userName)
-        }
-        //display my greetings
-        var displayMyGreetings = await Greetings.greet(userName, language)
-            // Greetings.setName(userName)
-    }
-    //console.log({ counter, displayMyGreetings })
-    res.render("index", {
-        display: displayMyGreetings,
-        count: counter
-    });
-})
+app.get("/counter/:userName", GreetingsRoutes.eachUser);
 
-app.get("/greeted", async function(req, res) {
-    var names = await Greetings.getNames();
-
-    res.render("greet", { name: names });
-})
-
-app.get("/counter/:userName", async function(req, res) {
-    const userName = req.params.userName
-    var namesList = await Greetings.greetedUsersCount(userName)
-    let msg = 'Hello, ' + userName + ' has been greeted ' + namesList + ' times '
-    res.render("counter", {
-        message: msg
-    });
-})
-
-app.get("/reset", async function(req, res) {
-    await Greetings.clearValues()
-    res.redirect("/")
-
-})
+app.get("/reset", GreetingsRoutes.Reset);
 
 let PORT = process.env.PORT || 3080;
 
